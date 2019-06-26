36fb1eb54b9ce1aa33bb97235ab9c59f459a7d9d
Koto Furumiya <kotofurumiya@gmail.com>
2019-06-26 11:15:59 +0900

    SWでのリソース取得をオンライン優先に変更（繋がらない場合だけキャッシュを使う）

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -34,14 +34,7 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if(response) {
-          return response;
-        }
-
-        return fetch(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
 
